5660c95c46522b9fe52e9746fdd8ede6e172effd
Mathieu Méa <mathieu.mea@gmail.com>
2017-04-29 09:03:24 -0400

    Compatibility with latest update.

--- src/org/mtransit/parser/ca_regina_transit_bus/ReginaTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_regina_transit_bus/ReginaTransitBusAgencyTools.java
@@ -1,22 +1,32 @@
 package org.mtransit.parser.ca_regina_transit_bus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Pair;
+import org.mtransit.parser.SplitUtils;
 import org.mtransit.parser.Utils;
+import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
+import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MDirectionType;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
+import org.mtransit.parser.mt.data.MTripStop;
 
 // http://openregina.cloudapp.net/
 // https://opengis.regina.ca/reginagtfs/google_transit.zip
@@ -95,115 +105,205 @@ public class ReginaTransitBusAgencyTools extends DefaultAgencyTools {
 		return AGENCY_COLOR;
 	}
 
-	private static final String EAST = "east";
-	private static final String WEST = "west";
-	private static final String NORTH = "north";
-	private static final String SOUTH = "south";
-
-	private static final String DIEPPE = "Dieppe";
+	private static final String ARCOLA_EAST = "Arcola East";
 	private static final String ARGYLE_PARK = "Argyle Pk";
-	private static final String GLENCAIRN = "Glencairn";
-	private static final String EAST_THS = "East";
+	private static final String EAST = "East";
 	private static final String DOWNTOWN = "Downtown";
-	private static final String SHERWOOD = "Sherwood";
+	private static final String HARBOUR_LANDING = "Harbour Lndg";
+	private static final String UNIVERSITY = "University";
+	private static final String UPLANDS = "Uplands";
+	private static final String WOODLAND_GROVE = "Woodland Grv";
+
+	private static HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
+	static {
+		HashMap<Long, RouteTripSpec> map2 = new HashMap<Long, RouteTripSpec>();
+		map2.put(2L, new RouteTripSpec(2L, //
+				0, MTrip.HEADSIGN_TYPE_STRING, WOODLAND_GROVE, //
+				1, MTrip.HEADSIGN_TYPE_STRING, ARGYLE_PARK) //
+				.addTripSort(0, //
+						Arrays.asList(new String[] { //
+						"1432", // STOCKTON ST @ CHILD AVE (SB)
+								"807", // 14TH AVE @ COCHRANE HIGH (EB)
+								"528", // WESTFAIR RD @ SUPERSTORE (WB)
+						})) //
+				.addTripSort(1, //
+						Arrays.asList(new String[] { //
+						"528", // WESTFAIR RD @ SUPERSTORE (WB)
+								"1545", // 11TH AVE @ LORNE ST (WB)
+								"144", // SANGSTER BLVD @ STERN BAY (EB)
+								"1432", // STOCKTON ST @ CHILD AVE (SB)
+						})) //
+				.compileBothTripSort());
+		map2.put(5L, new RouteTripSpec(5L, //
+				0, MTrip.HEADSIGN_TYPE_STRING, UPLANDS, //
+				1, MTrip.HEADSIGN_TYPE_STRING, DOWNTOWN) //
+				.addTripSort(0, //
+						Arrays.asList(new String[] { //
+						"1545", // 11TH AVE @ LORNE ST (WB)
+								"1536", // != 6TH AVE N @ GARNET ST (EB)
+								"488", // <> != ARGYLE ST N @ 6TH AVE N (NB)
+								"1454", // == 6TH AVE N @ ANGUS RD (EB)
+								"69", // STURDY ST @ 12TH AVE N (NB)
+						})) //
+				.addTripSort(1, //
+						Arrays.asList(new String[] { //
+						"69", // STURDY ST @ 12TH AVE N (NB)
+								"88", // 7TH AVE N @ SMITH ST (WB)
+								"1455", // == 6TH AVE N @ SHEPHERD ST (WB)
+								"488", // <> != ARGYLE ST N @ 6TH AVE N (NB)
+								"471", // == GARNET ST @ WOODWARD AVE (SB)
+								"1545", // 11TH AVE @ LORNE ST (WB)
+						})) //
+				.compileBothTripSort());
+		map2.put(15L, new RouteTripSpec(15L, //
+				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.EAST.getId(), //
+				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.WEST.getId()) //
+				.addTripSort(MDirectionType.EAST.intValue(), //
+						Arrays.asList(new String[] { //
+						"1537", // 11TH AVE @ SCARTH ST (EB)
+								"1182", // WINNIPEG ST @ 13TH AVE (SB)
+								"1189", // ROSE ST @ 13TH AVE (NB)
+						})) //
+				.addTripSort(MDirectionType.WEST.intValue(), //
+						Arrays.asList(new String[] { //
+						"1189", // ROSE ST @ 13TH AVE (NB)
+								"1537", // 11TH AVE @ SCARTH ST (EB)
+						})) //
+				.compileBothTripSort());
+		map2.put(16L, new RouteTripSpec(16L, //
+				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.EAST.getId(), //
+				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.WEST.getId()) //
+				.addTripSort(MDirectionType.EAST.intValue(), //
+						Arrays.asList(new String[] { //
+						"202", // ROCHDALE BLVD @ SHERWOOD MALL (EB)
+								"1574", // MCEACHERN DR @ MAZURAK CRES (WB)
+						})) //
+				.addTripSort(MDirectionType.WEST.intValue(), //
+						Arrays.asList(new String[] { //
+						"1574", // MCEACHERN DR @ MAZURAK CRES (WB)
+								"202", // ROCHDALE BLVD @ SHERWOOD MALL (EB)
+						})) //
+				.compileBothTripSort());
+		map2.put(17L, new RouteTripSpec(17L, //
+				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.EAST.getId(), //
+				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.WEST.getId()) //
+				.addTripSort(MDirectionType.EAST.intValue(), //
+						Arrays.asList(new String[] { //
+						"1450", // MAPLERIDGE DR @ MAPLETON BAY (SB)
+								"1589", // VANSTONE ST @ BIG BEAR BLVD (SB)
+						})) //
+				.addTripSort(MDirectionType.WEST.intValue(), //
+						Arrays.asList(new String[] { //
+						"1589", // VANSTONE ST @ BIG BEAR BLVD (SB)
+								"323", // ROCHDALE BLVD @ LAKEWOOD CRT (WB)
+								"1450", // MAPLERIDGE DR @ MAPLETON BAY (SB)
+						})) //
+				.compileBothTripSort());
+		map2.put(18L, new RouteTripSpec(18L, //
+				0, MTrip.HEADSIGN_TYPE_STRING, UNIVERSITY, //
+				1, MTrip.HEADSIGN_TYPE_STRING, HARBOUR_LANDING) //
+				.addTripSort(0, //
+						Arrays.asList(new String[] { //
+						"1566", // HARVARD WAY @ GRASSLANDS DR
+								"382", // UNIVERSITY DR E @ FIRST NATIONS WAY (NB)
+						})) //
+				.addTripSort(1, //
+						Arrays.asList(new String[] { //
+						"382", // UNIVERSITY DR E @ FIRST NATIONS WAY (NB)
+								"1397", // PARLIAMENT AVE @ HARBOUR LANDING DRIVE (WB)
+								"1566", // HARVARD WAY @ GRASSLANDS DR
+						})) //
+				.compileBothTripSort());
+		map2.put(22L, new RouteTripSpec(22L, //
+				0, MTrip.HEADSIGN_TYPE_STRING, UNIVERSITY, //
+				1, MTrip.HEADSIGN_TYPE_STRING, ARCOLA_EAST) //
+				.addTripSort(0, //
+						Arrays.asList(new String[] { //
+						"830", // WOODHAMS RD@SS LEISURE CENTRE (EB)
+								"831", // ASSINIBOINE AVE @ PRINCE OF WALES (WB)
+								"270", // UNIVERSITY DR W @ RIDDELL CENTRE (NB)
+								"587", // MASSEY RD @ PARKER AVE (SB)
+						})) //
+				.addTripSort(1, //
+						Arrays.asList(new String[] { //
+						"587", // MASSEY RD @ PARKER AVE (SB)
+								"249", // UNIVERSITY DR W @ RIDDELL CENTRE (SB)
+								"830", // WOODHAMS RD@SS LEISURE CENTRE (EB)
+						})) //
+				.compileBothTripSort());
+		map2.put(40L, new RouteTripSpec(40L, //
+				MDirectionType.NORTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.NORTH.getId(), //
+				MDirectionType.SOUTH.intValue(), MTrip.HEADSIGN_TYPE_DIRECTION, MDirectionType.SOUTH.getId()) //
+				.addTripSort(MDirectionType.NORTH.intValue(), //
+						Arrays.asList(new String[] { //
+						"1566", // HARVARD WAY @ GRASSLANDS DR (NB)
+								"1423", // STOCKTON ST @ CHILD AVE (NB)
+						})) //
+				.addTripSort(MDirectionType.SOUTH.intValue(), //
+						Arrays.asList(new String[] { //
+						"1423", // STOCKTON ST @ CHILD AVE (NB)
+								"1429", // DIEFENBAKER DR @ BLAKE RD (EB)
+								"1566", // HARVARD WAY @ GRASSLANDS DR (NB)
+						})) //
+				.compileBothTripSort());
+		map2.put(50L, new RouteTripSpec(50L, //
+				0, MTrip.HEADSIGN_TYPE_STRING, EAST, //
+				1, MTrip.HEADSIGN_TYPE_STRING, DOWNTOWN) //
+				.addTripSort(0, //
+						Arrays.asList(new String[] { //
+						"1544", // 11TH AVE @ CORNWALL ST (EB)
+								"528", // WESTFAIR RD @ SUPERSTORE (WB)
+						})) //
+				.addTripSort(1, //
+						Arrays.asList(new String[] { //
+						"528", // xx WESTFAIR RD @ SUPERSTORE (WB)
+								"529", // xx
+								"1372", // !=
+								"1420", // DEWDNEY AVE @ PRINCE OF WALES DR (EB)
+								"1267", // !=
+								"528", // xx WESTFAIR RD @ SUPERSTORE (WB)
+								"529", // xx
+								"530", // !=
+								"1544", // 11TH AVE @ CORNWALL ST (EB)
+						})) //
+				.compileBothTripSort());
+		ALL_ROUTE_TRIPS2 = map2;
+	}
+
+	@Override
+	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
+		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+		}
+		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+	}
+
+	@Override
+	public ArrayList<MTrip> splitTrip(MRoute mRoute, GTrip gTrip, GSpec gtfs) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
+		}
+		return super.splitTrip(mRoute, gTrip, gtfs);
+	}
+
+	@Override
+	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+		}
+		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
+	}
 
 	@Override
 	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		if (mRoute.getId() == 1l) {
-			if (gTrip.getDirectionId() == 0) {
-				mTrip.setHeadsignString(DIEPPE, gTrip.getDirectionId());
-				return;
-			}
-		} else if (mRoute.getId() == 2l) {
-			if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignString(ARGYLE_PARK, gTrip.getDirectionId());
-				return;
-			}
-		} else if (mRoute.getId() == 3l) {
-			if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignString(SHERWOOD, gTrip.getDirectionId());
-				return;
-			}
-		} else if (mRoute.getId() == 15l) {
-			String gTripHeadsignLC = gTrip.getTripHeadsign().toLowerCase(Locale.ENGLISH);
-			if (gTripHeadsignLC.endsWith(EAST)) {
-				mTrip.setHeadsignDirection(MDirectionType.EAST);
-				return;
-			} else if (gTripHeadsignLC.endsWith(WEST)) {
-				mTrip.setHeadsignDirection(MDirectionType.WEST);
-				return;
-			}
-		} else if (mRoute.getId() == 16l) {
-			if (gTrip.getDirectionId() == 0) {
-				mTrip.setHeadsignDirection(MDirectionType.EAST);
-				return;
-			} else if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignDirection(MDirectionType.WEST);
-				return;
-			}
-		} else if (mRoute.getId() == 17l) {
-			String gTripHeadsignLC = gTrip.getTripHeadsign().toLowerCase(Locale.ENGLISH);
-			if (gTripHeadsignLC.endsWith(EAST)) {
-				mTrip.setHeadsignDirection(MDirectionType.EAST);
-				return;
-			} else if (gTripHeadsignLC.endsWith(WEST)) {
-				mTrip.setHeadsignDirection(MDirectionType.WEST);
-				return;
-			}
-		} else if (mRoute.getId() == 21l) {
-			if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignString(GLENCAIRN, gTrip.getDirectionId());
-				return;
-			}
-		} else if (mRoute.getId() == 22l) {
-			if (gTrip.getDirectionId() == 0) {
-				mTrip.setHeadsignString("University", gTrip.getDirectionId());
-				return;
-			} else if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignString("Arcola East", gTrip.getDirectionId());
-				return;
-			}
-		} else if (mRoute.getId() == 40l) {
-			String gTripHeadsignLC = gTrip.getTripHeadsign().toLowerCase(Locale.ENGLISH);
-			if (gTripHeadsignLC.contains(NORTH)) {
-				mTrip.setHeadsignDirection(MDirectionType.NORTH);
-				return;
-			} else if (gTripHeadsignLC.contains(SOUTH)) {
-				mTrip.setHeadsignDirection(MDirectionType.SOUTH);
-				return;
-			}
-			if (gTrip.getDirectionId() == 0) {
-				mTrip.setHeadsignDirection(MDirectionType.SOUTH);
-				return;
-			} else if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignDirection(MDirectionType.NORTH);
-				return;
-			}
-		} else if (mRoute.getId() == 50l) {
-			if (gTrip.getDirectionId() == 0) {
-				mTrip.setHeadsignString(EAST_THS, gTrip.getDirectionId());
-				return;
-			} else if (gTrip.getDirectionId() == 1) {
-				mTrip.setHeadsignString(DOWNTOWN, gTrip.getDirectionId());
-				return;
-			}
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return; // split
 		}
 		mTrip.setHeadsignString(cleanTripHeadsign(gTrip.getTripHeadsign()), gTrip.getDirectionId());
 	}
 
 	@Override
 	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
-		if (mTrip.getRouteId() == 4l) {
-			if (mTrip.getHeadsignId() == 1) {
-				mTrip.setHeadsignString("Walsh Acres", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 5l) {
-			if (mTrip.getHeadsignId() == 1) {
-				mTrip.setHeadsignString("Downtown", mTrip.getHeadsignId());
-				return true;
-			}
-		}
 		System.out.printf("\nUnexpected trips to merge %s & %s\n", mTrip, mTripToMerge);
 		System.exit(-1);
 		return false;
